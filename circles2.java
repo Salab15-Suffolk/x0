c1c5f6897527eb540923be45bcdae1b09bced543
Salab15-Suffolk <Salab15@mail.sunysuffolk.edu>
2015-09-09 12:13:56 -0400

    Update circles2.java
    Changed shape of ellipse and named it Herman

--- circles2.java
+++ circles2.java
@@ -1,24 +1,27 @@
 //// Wild circles #2.
-String title=  "Click for another circle.";
-String subtitle=  "Press any key to erase.";
+String title= "Click for another circle. ";
+String Name= "HERMAN" ;
+String subtitle=  "Press any key to erase";
 String author=  "Bruce Alan Martin";
 
 // GLOBAL VARIABLES //
-float x,y;
+  
+float x,y; 
 int h,w;
-
 // Setup:  screen size, initialization. //
 void setup() {
   size(500, 500);
   x=  width/2;
   y=  height/2;
-  w=  80;          // width of ellipse.
+  w=  150;          // width of ellipse.
   h=  80;          // height of ellipse
+  
 }
 
 // Next frame. //
 void draw() {
-  text( title, 10, 10 );
+  text (title, 10,10);
+  text( Name, 230, 200);
   text( subtitle, width/2, 10 );
   text( author, 10, height-10 );
   text( w+"x"+h, width/2,height-10 );
@@ -37,8 +40,8 @@ void mousePressed() {
 //  Handle keyboard //
 void keyPressed() {
   // Erase everything, change size & color. //
-  background( random(255), random(255), random(255) );
-  fill( random(255), random(255), random(255) );
+  background(random(255), random(255), random(255) );
+  fill( (255), random(255), random(255), random(255)  );
   w=  int( random(50,150) );
   h=  int( random(50,150) );
 }
