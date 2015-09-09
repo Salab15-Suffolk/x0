8a38910317406e6bb8fa582f9cc75335cab73bdc
Salab15-Suffolk <Salab15@mail.sunysuffolk.edu>
2015-09-09 10:38:05 -0400

    Update circles0.java

--- circles0.java
+++ circles0.java
@@ -2,10 +2,10 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(300, 400);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 100,250, 40,90 );       // Try changing the (x,y) and size.
 }
